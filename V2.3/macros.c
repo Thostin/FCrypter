7e85355f3417a241a21932e61dca03d3b1280d5d
Thostin <everortega283@gmail.com>
2023-09-06 11:11:05 -0400

    volviendo a corregir el tema del este de windows

--- V2.3/macros.c
+++ V2.3/macros.c
@@ -13,10 +13,12 @@
 #define PasswordLen(A) A->PasswordMax-A->PasswordMin+1
 // Windows commands
 
-// #define CLEAR "cls"
-
+#define CLEAR "cls"
+#define PAUSE "pause"
 
 
 // Linux commands
 
-#define CLEAR "clear"
+// #define CLEAR "clear"
+// #define PAUSE "cd ."
+
